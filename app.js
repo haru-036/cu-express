0455414a36ae9d50ea4c78306650ab213819208b
haru <hinagoro5656@gmail.com>
2026-06-07 15:21:15 +0900

    MongoDBと接続

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var noteRouter = require('./routes/notes');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/notes', noteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/notes.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+const { MongoClient } = require('mongodb');
+const uri = "";
+const client = new MongoClient(uri);
+
+router.get("/", async (req, res, next) => {
+  const database = client.db('notes');
+  const notes = database.collection("notes");
+
+  const query = { id: 2 };
+  const note = await notes.findOne(query);
+
+  res.json(note);
+});
+
+module.exports = router;
