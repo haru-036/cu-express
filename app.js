d4c41171faa9dfa8755cb4801f1f753829cf7247
haru <hinagoro5656@gmail.com>
2026-06-07 15:43:56 +0900

    helloルートを追加し、Helloページを表示する機能を実装

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
 var noteRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
 app.use('/notes', noteRouter);
 app.use('/cat', catRouter);
 
